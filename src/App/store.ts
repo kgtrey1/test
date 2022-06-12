aaca76c2cb76ffa80bfdaf473bf2e9fbd2e4521b
Kevin Gouyet <kevin.gouyet@frenchappweb.com>
2022-06-12 17:27:15 +0200

    add: devtools

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,10 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userSlice from 'Reducers/userSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import userSlice from 'Reducers/userSlice'
+import devToolsEnhancer from 'remote-redux-devtools'
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
-});
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+    reducer: {
+        user: userSlice,
+    },
+    enhancers:
+        process.env.NODE_ENV === 'development'
+            ? [
+                  devToolsEnhancer({
+                      realtime: true,
+                      name: 'ERISE ',
+                      hostname: 'localhost',
+                      port: 8000,
+                  }),
+              ]
+            : [],
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
