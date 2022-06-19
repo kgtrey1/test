476ed5f6758c7e3a18fd22600ecf8c3f292b493e
Kevin Gouyet <kevin.gouyet@frenchappweb.com>
2022-06-19 17:29:14 +0200

    fix: coding style

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,12 @@
-import { Home, NotFound } from 'Pages'
 import React from 'react'
+import { Home, NotFound } from 'Pages'
 import { Route, Routes } from 'react-router-dom'
 
-const App: React.FunctionComponent = (): JSX.Element => {
-    return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='*' element={<NotFound />} />
-        </Routes>
-    )
-}
+const App: React.FunctionComponent = (): JSX.Element => (
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+    </Routes>
+)
 
 export default App
