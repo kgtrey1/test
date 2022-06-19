ab2d9d4863fc9e87fe568adf3f227665df197c44
Kevin Gouyet <kevin.gouyet@frenchappweb.com>
2022-06-19 17:27:32 +0200

    add: authSlice

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from 'Reducers/userSlice'
+import authSlice from 'Reducers/authSlice'
 
 export const store = configureStore({
     reducer: {
+        auth: authSlice,
         user: userSlice,
     },
     devTools: true,
