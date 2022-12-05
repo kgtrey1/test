229acc3f8ac9e8a348d0b0d9024481001f464827
faez123 <faezdhuny786@gmail.com>
2022-12-05 14:53:19 +0300

    Add page GameDetails

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,5 @@
 import { Games, Home, NotFound, Team, UserProfile } from 'Pages'
+import GameDetails from 'Pages/GameDetails/GameDetails'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
@@ -9,6 +10,20 @@ const App: React.FunctionComponent = (): JSX.Element => {
             <Route path='/games' element={<Games />} />
             <Route path='/team' element={<Team />} />
             <Route path='*' element={<NotFound />} />
+            <Route
+                path='/games/rocketleague'
+                element={
+                    <GameDetails
+                        imageURL={
+                            'https://www.journaldugeek.com/content/uploads/2021/11/template-jdg-2021-11-30t111613-030.jpg'
+                        }
+                        description={
+                            'Lol is an online video game developed by Epic Games and released in 2017. It is available in three distinct game mode versions that otherwise share the same general gameplay and game engine..'
+                        }
+                        modes={['solo', 'duo', 'trio', 'squad']}
+                    />
+                }
+            />
             <Route
                 path='/userProfile'
                 element={
---
+++ src/Pages/GameDetails/GameDetails.tsx
@@ -0,0 +1,103 @@
+import { Grid, Typography } from '@mui/material'
+import { Page } from 'Lib/Pages'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import React from 'react'
+
+interface IGameDetails {
+    imageURL: string
+    description: string
+    modes: Array<string>
+}
+
+const GameDetails: React.FC<IGameDetails> = (props): JSX.Element => {
+    const { imageURL, description, modes } = props
+    const [mode, setMode] = React.useState('')
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setMode(event.target.value as string)
+    }
+
+    return (
+        <Page>
+            <Grid
+                container
+                direction='column'
+                alignItems='center'
+                style={{ background: 'none' }}>
+                <Grid item>
+                    <img
+                        src={imageURL}
+                        height='200vh'
+                        width='400vw'
+                        style={{ borderRadius: '18%' }}></img>
+                </Grid>
+                <Grid item marginTop='5vh'>
+                    <Typography
+                        color='white'
+                        fontFamily='Roboto-Regular'
+                        fontSize='30px'>
+                        Description
+                    </Typography>
+                </Grid>
+                <Grid item width='100vh' marginTop='5vh'>
+                    <Typography
+                        align='justify'
+                        color='white'
+                        fontFamily='Roboto-Regular'
+                        fontSize='20px'>
+                        {description}
+                    </Typography>
+                </Grid>
+                <Grid item marginTop='5vh'>
+                    <Grid
+                        container
+                        direction='column'
+                        alignItems='left'
+                        width='100vh'>
+                        <Grid item>
+                            <Typography
+                                color='white'
+                                fontFamily='Roboto-Regular'
+                                fontSize='30px'>
+                                Mode
+                            </Typography>
+                        </Grid>
+                        <Grid item marginTop='5vh' width='20vh'>
+                            <Box sx={{ minWidth: 120 }}>
+                                <FormControl fullWidth>
+                                    <InputLabel style={{ color: 'white' }}>
+                                        Select a mode
+                                    </InputLabel>
+                                    <Select
+                                        value={mode}
+                                        label='Mode'
+                                        sx={{
+                                            '& .MuiSelect-select': {
+                                                borderColor: 'white',
+                                            },
+                                            '& .MuiTab-root.Mui-selected': {
+                                                borderColor: 'white',
+                                            },
+                                        }}
+                                        onChange={handleChange}>
+                                        {modes.map((mode) => (
+                                            <MenuItem key={mode} value={mode}>
+                                                {mode}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Page>
+    )
+}
+
+export default GameDetails
